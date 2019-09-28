48b6774d88dfec8d4fb967820218826df1ee0817
ecki-d <support@kaizenmedia.co.za>
2019-09-28 09:11:27 +0200

    creating a page and building

--- README.md
+++ README.md
@@ -1,22 +1,3 @@
-# dropchow
+## Kaizen Codes NuxtJS Youtube video course.
 
-> Order, eat.
-
-## Build Setup
-
-``` bash
-# install dependencies
-$ yarn install
-
-# serve with hot reload at localhost:3000
-$ yarn dev
-
-# build for production and launch server
-$ yarn build
-$ yarn start
-
-# generate static project
-$ yarn generate
-```
-
-For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+> Typed version coming soon.
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,174 @@
+<!-- 
+  A page in NuxtJS can be thought of as two things:
+    1. A route in the URL bar
+    2. A fully generated .html page
+-->
+
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        My Contact Book
-      </h1>
-      <h2 class="subtitle">
-        A place where I can never lose my contacts.
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <section id="home-page">
+    <div class="home-header">
+      <img
+        width="80px"
+        src="~/assets/address-card-solid.svg"
+        alt="My Contact Keeper Icon"
+      />
+      <h1>My Contact Keeper</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      </p>
+    </div>
+    <div class="home-contacts">
+      <div class="container">
+        <div class="row py-3">
+          <div class="col-md-6 mx-auto my-3">
+            <div class="contact-card">
+              <img
+                width="60px"
+                height="60px"
+                src="https://www.w3schools.com/howto/img_avatar2.png"
+                alt="Contact Name and Number"
+                class="contact-avatar"
+              />
+              <div class="contact-details">
+                <h1 class="contact-name">Jane Doe</h1>
+                <p class="contact-email">jane.doe@enterprise.com</p>
+              </div>
+              <div class="contact-call">
+                <a href="tel:+264814774491" class="call-link">
+                  <img
+                    width="25px"
+                    height="25px"
+                    src="~/assets/phone-solid.svg"
+                    alt="Telephone Icon"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 mx-auto my-3">
+            <div class="contact-card">
+              <img
+                width="60px"
+                height="60px"
+                src="https://www.w3schools.com/howto/img_avatar2.png"
+                alt="Contact Name and Number"
+                class="contact-avatar"
+              />
+              <div class="contact-details">
+                <h1 class="contact-name">Jane Doe</h1>
+                <p class="contact-email">jane.doe@enterprise.com</p>
+              </div>
+              <div class="contact-call">
+                <a href="tel:+264814774491" class="call-link">
+                  <img
+                    width="25px"
+                    height="25px"
+                    src="~/assets/phone-solid.svg"
+                    alt="Telephone Icon"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 mx-auto my-3">
+            <div class="contact-card">
+              <img
+                width="60px"
+                height="60px"
+                src="https://www.w3schools.com/howto/img_avatar2.png"
+                alt="Contact Name and Number"
+                class="contact-avatar"
+              />
+              <div class="contact-details">
+                <h1 class="contact-name">Jane Doe</h1>
+                <p class="contact-email">jane.doe@enterprise.com</p>
+              </div>
+              <div class="contact-call">
+                <a href="tel:+264814774491" class="call-link">
+                  <img
+                    width="25px"
+                    height="25px"
+                    src="~/assets/phone-solid.svg"
+                    alt="Telephone Icon"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 mx-auto my-3">
+            <div class="contact-card">
+              <img
+                width="60px"
+                height="60px"
+                src="https://www.w3schools.com/howto/img_avatar2.png"
+                alt="Contact Name and Number"
+                class="contact-avatar"
+              />
+              <div class="contact-details">
+                <h1 class="contact-name">Jane Doe</h1>
+                <p class="contact-email">jane.doe@enterprise.com</p>
+              </div>
+              <div class="contact-call">
+                <a href="tel:+264814774491" class="call-link">
+                  <img
+                    width="25px"
+                    height="25px"
+                    src="~/assets/phone-solid.svg"
+                    alt="Telephone Icon"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-
-export default {
-  components: {
-    Logo
-  }
-};
+export default {};
 </script>
 
 <style>
-.container {
+.home-header {
+  padding: 2em;
+  background: #f4f4f4;
+  text-align: center;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
+}
+
+.home-contacts {
+  max-width: 800px;
   margin: 0 auto;
-  min-height: 100vh;
+}
+
+.contact-card {
+  padding: 2em 0;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-around;
   align-items: center;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  max-width: 425px;
+  margin: 0 auto;
+}
+
+.contact-avatar {
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: center;
+}
+
+.contact-call,
+.contact-details > * {
   text-align: center;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+@media screen and (max-width: 500px) {
+  .contact-card {
+    max-width: calc(90% - 1em);
+  }
 }
 </style>
